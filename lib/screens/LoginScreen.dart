fdda8358a399da605934de0a8716e48680a4e466
Raymond Douglas Wamani <wamaniray@gmail.com>
2023-02-07 17:21:52 +0300

    Add files via upload

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -13,35 +13,39 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  String _email='', _password='';
+  //String passwordField;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
           child: Container(
-            margin: EdgeInsets.only(top: 120),
-            padding: EdgeInsets.only(left: 30,right: 30),
-            child: Column(
-              
-        children: [
-            
+        margin: EdgeInsets.only(top: 100),
+        padding: EdgeInsets.only(left: 30, right: 30),
+        child: Column(
+          children: [
             Container(
-            width: 100.0,
-            height: 100.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue
-            ),
-            child: Center(
-              child: Text('SPZ',
-              style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25
+              width: 100.0,
+              height: 100.0,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
+              child: Center(
+                child: Text(
+                  'SPZ',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25),
+                ),
               ),
             ),
-            ),
-          ),
 
-          SizedBox(height: 40,),
-    
+            SizedBox(
+              height: 40,
+            ),
+
             Container(
               alignment: Alignment.topLeft,
               child: Text(
@@ -63,6 +67,12 @@ class _LoginState extends State<Login> {
                 children: [
                   FormBuilderTextField(
                     name: "user_name",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Text Field cannot be empty";
+                      }
+                      onSaved:(value) => _email = value;
+                    },
                     textInputAction: TextInputAction.next,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
@@ -70,7 +80,7 @@ class _LoginState extends State<Login> {
                       ),
                       labelText: "User Name",
                       suffixIcon: Icon(Icons.person, color: Colors.blue[700]),
-                      hintText: "draymond",
+                      // hintText: "draymond",
                     ),
                   ),
                   SizedBox(
@@ -79,6 +89,25 @@ class _LoginState extends State<Login> {
                   FormBuilderTextField(
                       name: 'password',
                       obscureText: true,
+                      validator: (value) {
+                      // if (value !=
+                      //     _formKey?.currentState?.fields["password"]
+                      //         ?.currentState?.value) {
+                      //   return "Passwords don't match";
+                      // }
+                      //},
+
+                        if (value == null || value.isEmpty) {
+                          return "Password can not be empty";
+                        }
+                        if (value.length < 8) {
+                          return "Password must be atleast 8 characters";
+                        }
+                      },
+                      onSaved: (value) => _password = value as String,
+                      //if (value != _formKey.currentState.fields["password"].currentState.value) {
+                      // return "Passwords don't match";
+
                       textInputAction: TextInputAction.done,
                       decoration: InputDecoration(
                           border: OutlineInputBorder(
@@ -97,32 +126,44 @@ class _LoginState extends State<Login> {
                     height: 50,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DashBoard(),
-                              ),
-                            );
+                          if (_formKey.currentState?.validate() == true) {
+                           _formKey.currentState?.save();
+                           Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DashBoard(),
+                          ),
+                        );
+                          print("Email: $_email, Password: $_password");
+                    // Perform login process
+                  }
+                        //if (_formKey.currentState?.validate() == true) {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => DashBoard(),
+                        //   ),
+                        // );
                       },
+                      //},
                       child: Text(
                         "LOGIN",
                       ),
-                      style: ElevatedButton.styleFrom(),
+                      //style: ElevatedButton.styleFrom(),
                     ),
                   ),
                   SizedBox(
                     height: 27.0,
                   ),
                   InkWell(
-                    onTap: (){
-                        // Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //       builder: (context) => ForgotPassword(),
-                        //     ),
-                        //   );
+                    onTap: () {
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (context) => ForgotPassword(),
+                      //     ),
+                      //   );
                     },
-                        
                     child: Text(
                       "Forgot Password?",
                       style: TextStyle(
@@ -168,9 +209,9 @@ class _LoginState extends State<Login> {
             //padding: const EdgeInsets.all(15.0),
 
             // ),
-        ],
-      ),
-          )),
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/screens/SignUpScreen.dart
+++ lib/screens/SignUpScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:stock_app/screens/DashboardScreen.dart';
 import 'package:stock_app/screens/LoginScreen.dart';
 
 class SignUp extends StatefulWidget {
@@ -12,35 +13,37 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         body: SingleChildScrollView(
       child: Container(
-        margin: EdgeInsets.only(top: 120),
-        padding: EdgeInsets.only(left: 30,right: 30),
-        child: Column(
-          children: [
+        margin: EdgeInsets.only(top: 80),
+        padding: EdgeInsets.only(left: 30, right: 30),
+        child: Column(children: [
           //---header begin--
-          
+
           Container(
             width: 100.0,
             height: 100.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue
-            ),
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
             child: Center(
-              child: Text('SPZ',
-              style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25
+              child: Text(
+                'SPZ',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25),
               ),
-            ),
             ),
           ),
 
-          SizedBox(height: 40,),
+          SizedBox(
+            height: 40,
+          ),
 
           Container(
             alignment: Alignment.topLeft,
@@ -50,109 +53,138 @@ class _SignUpState extends State<SignUp> {
             ),
           ),
 
-          SizedBox(height: 30.0,),
+          SizedBox(
+            height: 30.0,
+          ),
 
-          Container(
-            //padding: EdgeInsets.all(30),
+          FormBuilder(
+            key: _formKey,
             child: Column(
               children: [
-                
                 FormBuilderTextField(
-                name: 'Full Name',
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
+                  //key: _nameFieldKey,
+                  name: 'Full Name',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Text Field cannot be empty";
+                    }
+                  },
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(
                         //borderSide: BorderSide(color: Colors.blue),
                         borderRadius: BorderRadius.all(Radius.circular(12.0))),
                     label: Text('Name'),
                     //hintText: "",
-                    )
-                    ),
+                  ),
+                  //validator: FormBuilderValidators.compose(value) {
 
-                SizedBox(height: 30.0,),
-
-                FormBuilderTextField(
-                name: 'Email',
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                      borderRadius: BorderRadius.all(Radius.circular(12.0))),
-                  label: Text('Email'),
-                  hintText: "name@example.com",
-                )
+                  //},
                 ),
-
-                SizedBox(height: 30.0,),
-
+                SizedBox(
+                  height: 30.0,
+                ),
                 FormBuilderTextField(
-                name: 'Password',
-                obscureText: true,
-                textInputAction: TextInputAction.done,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                        borderRadius: BorderRadius.all(Radius.circular(12.0))),
-                    labelText: 'Password',
-                    hintText: "min. 8 characters",
-                    suffixIcon: Icon(Icons.visibility)
-                    )
-                    ),
-
-                SizedBox(height: 27.0,),
-
+                    name: 'Email',
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Email cannot be empty";
+                      }
+                      if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                        return "Invalid email address";
+                      }
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(12.0))),
+                      label: Text('Email'),
+                      hintText: "name@example.com",
+                    )),
+                SizedBox(
+                  height: 30.0,
+                ),
+                FormBuilderTextField(
+                    name: 'password',
+                    //validator: FormBuilderValidators.required(),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Password can not be empty";
+                      }
+                      if (value.length < 8) {
+                        return "Password must be atleast 8 characters";
+                      }
+                    },
+                    obscureText: true,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0))),
+                        labelText: 'Password',
+                        hintText: "min. 8 characters",
+                        suffixIcon: Icon(Icons.visibility))),
+                SizedBox(
+                  height: 27.0,
+                ),
                 Container(
-                  width:500 ,
+                  width: 500,
                   height: 50,
                   child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Sign Up'),
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() == true) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Login(),
+                          ),
+                        );
+                      }
+                    },
+                    child: Text('Sign Up'),
                     //style: ElevatedButton.styleFrom(),
                   ),
                 ),
-
-                SizedBox(height: 120.0,),
-
+                SizedBox(
+                  height: 120.0,
+                ),
                 Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Text(
-                  'Have an account already?',
-                  style: TextStyle(fontSize: 15.0),
-                ),
-
-                InkWell(
-                onTap: (() {
-                  Navigator.push(
-                    context,MaterialPageRoute(
-                      builder: (context) => Login(),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Have an account already?',
+                      style: TextStyle(fontSize: 15.0),
                     ),
-                  );
-                }),
-                child: Text(
-                  'Log In',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 8, 132, 234),
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold),
-                ),
+                    InkWell(
+                      onTap: (() {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Login(),
+                          ),
+                        );
+                      }),
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 8, 132, 234),
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
                 )
-                ],
-                )
-
-
               ],
             ),
           )
-
-          
-          
-          
         ]),
       ),
-    )
-
-    );
+    ));
   }
 }
